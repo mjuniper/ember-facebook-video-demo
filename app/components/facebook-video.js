7461e3c7b5b18eeba219b83e3932f3225c0ca898
mjuniper <mjuniper@gmail.com>
2018-08-28 09:45:49 -0600

    facebook-video component

---
+++ app/components/facebook-video.js
@@ -0,0 +1,49 @@
+import { reads } from '@ember/object/computed';
+import Component from '@ember/component';
+
+/*
+  NOTE:
+    * https://developers.facebook.com/docs/plugins/embedded-video-player/# indicates that we need to include an appId but it works without it....
+    * this is probably not fastboot friendly
+*/
+
+export default Component.extend({
+
+  classNames: [ 'fb-video' ],
+
+  attributeBindings: [ 'data-href', 'data-width', 'data-allowfullscreen' ],
+
+  allowFullscreen: true,
+  'data-allowfullscreen': reads('allowFullscreen'),
+
+  width: 'auto',
+  'data-width': reads('width'),
+
+  'data-href': reads('url'),
+
+  init() {
+    this._super(...arguments);
+    this.loadScript();
+  },
+
+  loadScript: function() {
+    // NOTE: apparently this will work even if this script lands after the element is rendered
+    const tagName = 'script';
+    const id = 'facebook-jssdk';
+    let js;
+    const fjs = document.getElementsByTagName(tagName)[0];
+    if (document.getElementById(id)) { return; }
+
+    // NOTE: the instructions (linked above) say you need to add this element but it does not seem to be necessary
+    // i think if it is not there the script adds it
+    const rootEl = document.createElement('div');
+    rootEl.id = 'fb-root';
+    document.body.append(rootEl);
+
+    js = document.createElement(tagName);
+    js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.1";
+    fjs.parentNode.insertBefore(js, fjs);
+  }
+
+});
